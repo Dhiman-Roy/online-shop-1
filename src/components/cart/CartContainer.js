10e3ef6492d13d6621d72083e6907c617b39f3c0
Dhiman-Roy <roy769982@gmail.com>
2023-09-01 22:47:17 +0600

    code added

--- src/components/cart/CartContainer.js
+++ src/components/cart/CartContainer.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
+import  ReactDOM  from 'react-dom';
 import Cart from './Cart'
 import styles from './CartContainer.module.css';
 
 export default function CartContainer() {
-  return (
-    <div className={styles.backdrop}>
+  return ReactDOM.createPortal(
+    <>
+    <div className={styles.backdrop}></div>
         <div className={styles.modal}>
             <h2>Your Shopping Cart</h2>
             <Cart/>
-        </div>
+        
     </div>
-  )
+    </>
+ ,document.getElementById('portal') )
 }
