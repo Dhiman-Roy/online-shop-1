e6d79ccd05d584bc43600143a59b99473b6319dd
Dhiman-Roy <roy769982@gmail.com>
2023-09-01 23:58:01 +0600

    code added

--- src/components/product/Products.js
+++ src/components/product/Products.js
@@ -1,7 +1,19 @@
 import style from './Products.module.css';
+import { productActions } from '../../store/product-slice';
+import { useDispatch } from 'react-redux';
 
 const Products = (props) => {
     const {title, description, image, quantity, price} = props;
+    const dispatch = useDispatch();
+    const addToCartHandler = () => {
+        dispatch(productActions.addItemToCart({
+            id,
+            title,
+            price,
+            quantity,
+            image
+        }))
+    }
     return (
         <div className={style.card}>
             <img className={style.img} src={image} alt='image'/>
@@ -9,7 +21,7 @@ const Products = (props) => {
                 <div className={style.title}>{title}</div>
                 <div>{price}$</div>
                 
-                <button className={style.addButton}>Add to cart</button>
+                <button onClick={addToCartHandler} className={style.addButton}>Add to cart</button>
             </div>
             
         </div>
